f3d365fbef679ec7aead1cde13f7bb081ec1e19f
Silas Owu <owusilasyp00@gmail.com>
2024-09-18 23:49:47 -0700

    🗃️ Feat: setup app store with localstorage integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { appReducers } from './store/reducers';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { ActionReducer, MetaReducer } from '@ngrx/store';
+
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys: ['theme', 'kanbanBoards'],
+    rehydrate: true
+  })(reducer);
+}
+
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    provideRouterStore(),
+    provideStore(appReducers, { metaReducers }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
   ]
 };
--- src/app/store/kanban/kanban.model.ts
+++ src/app/store/kanban/kanban.model.ts
@@ -1,7 +1,7 @@
 import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
 export interface Task {
-  id: string; // Add this
+  id: string; 
   title: string;
   description: string;
   status: 'Todo' | 'Doing' | 'Done';
@@ -9,21 +9,21 @@ export interface Task {
 }
 
 export interface Subtask {
-  id: string; // Add this
+  id: string;
   title: string;
   isCompleted: boolean;
 }
 
 export interface Column {
-  id: string; // Add this
+  id: string;
   name: string;
-  taskIds: string[]; // Change this to store task IDs instead of tasks
+  taskIds: string[];
 }
 
 export interface IBoard {
-  id: string; // Add this
+  id: string;
   name: string;
-  columnIds: string[]; // Change this to store column IDs instead of columns
+  columnIds: string[];
 }
 
 export interface KanbanState extends EntityState<IBoard> {
